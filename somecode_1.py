1c24f947d68685fc2b2d0f475c998fdbd3aba4aa
Vjacheslav <menshikov86@mail.ru>
2021-03-16 17:46:17 +0300

    somecode_1

---
+++ somecode_1.py
@@ -0,0 +1,16 @@
+class OwnError(Exception):  # создание дочернего класса обработки исключений
+    def __init__(self, txt):
+        self.txt = txt
+
+inp_data = input("Введите положительное число: ")
+
+try:
+    inp_data = int(inp_data)
+    if inp_data < 0:
+        raise OwnError("Вы ввели отрицательное число!")  # вызов класса обработки исключений
+except ValueError:
+    print("Вы ввели не число")
+except OwnError as err:  # обработка исключения
+    print(err)
+else:
+    print(f"Все хорошо. Ваше число: {inp_data}")
