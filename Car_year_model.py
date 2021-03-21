6364fa8bc6bf07bd3b9fa41a5fe26265818e355c
Slava# <72523263+vmenshikov106@users.noreply.github.com>
2021-03-21 13:36:39 +0300

    Add files via upload

---
+++ Car_year_model.py
@@ -0,0 +1,23 @@
+class Auto:
+
+    def __init__(self, year):
+        self.year = year
+
+    @property  # свойство года
+    def year(self):
+        return self.__year
+
+    @year.setter  # сеттер для создания свойств
+    def year(self, year):
+        if year < 2000:
+            self.__year = 2000
+        elif year > 2019:
+            self.__year = 2019
+        else:
+            self.__year = year
+
+    def get_auto_year(self):
+        return f"Автомобиль выпущен в {str(self.year)} году"
+
+a = Auto(2090)
+print(a.get_auto_year())
