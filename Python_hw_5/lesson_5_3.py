339b71acb4430978cb71d128b67bad6a05a81bd7
Slava# <72523263+vmenshikov106@users.noreply.github.com>
2021-04-26 11:32:13 +0300

    Update lesson_5_3.py

--- Python_hw_5/lesson_5_3.py
+++ Python_hw_5/lesson_5_3.py
@@ -4,17 +4,16 @@
 Выполнить подсчет средней величины дохода сотрудников.
 """
 
-# salarys = []
-#
-# with open('salary.txt', encoding='utf-8') as my_f:
-#     lines = my_f.readlines()
-#
-#     for line in lines:
-#         name, salary = line.split(" ")
-#         salary = int(salary)
-#         salarys.append(salary)
-#         if salary < 20000:
-#             print('Оклад менее 20 000 у сотрудников: ', name)
-#
-#     print("Средний доход сотрудников: ", round(sum(salarys) / len(salarys), 2))
+salarys = []
+
+with open('salary.txt', encoding='utf-8') as my_f:
+    lines = my_f.readlines()
+    for line in lines:
+        name, salary = line.split(" ")
+        salary = int(salary)
+        salarys.append(salary)
+        if salary < 20000:
+            print('Оклад менее 20 000 у сотрудников: ', name)
+            
+    print("Средний доход сотрудников: ", round(sum(salarys) / len(salarys), 2))
 
