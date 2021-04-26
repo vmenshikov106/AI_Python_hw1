283d919931ad296a1847c3a7f6dde0e073f972b0
Slava# <72523263+vmenshikov106@users.noreply.github.com>
2021-04-26 11:30:35 +0300

    Update lesson_5_2.py

--- Python_hw_5/lesson_5_2.py
+++ Python_hw_5/lesson_5_2.py
@@ -1,28 +1,29 @@
-"""
-Создать текстовый файл (не программно), сохранить в нем несколько строк,
+"""Создать текстовый файл (не программно), сохранить в нем несколько строк,
 выполнить подсчет количества строк, количества слов в каждой строке.
 """
+
+
 # Подсчёт количества строк в файле:
 
-# my_f = open("text_file.txt")
+my_f = open("text_file.txt")
 
-# count = 0
-# with my_f as f:
-#     for line in f:
-#         count += 1
-# print("Количество строк в файле: ", count)
-# my_f.close()
+count = 0
+with my_f as f:
+     for line in f:
+         count += 1
+print("Количество строк в файле: ", count) 
+my_f.close()
 
 # Подсчёт количества слов в каждой строке файла:
 
-# my_f = open("text_file.txt")
+my_f = open("text_file.txt")
 
-# count = 1
-# with my_f as f:
-#     for line in f:
-#         words = len(line.split())
-#         print(f"Количество слов в строке {count}: {words}")
-#         count += 1
+count = 1
+with my_f as f:
+    for line in f:
+          words = len(line.split())
+          print(f"Количество слов в строке {count}: {words}")
+          count += 1
 
-# my_f.close()
+my_f.close()
 
