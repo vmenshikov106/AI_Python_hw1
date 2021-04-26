5a2bc42b95935e114fc36c7409221815e627ebd4
Slava# <72523263+vmenshikov106@users.noreply.github.com>
2021-04-26 11:28:35 +0300

    Update lesson_5_1.py

--- Python_hw_5/lesson_5_1.py
+++ Python_hw_5/lesson_5_1.py
@@ -3,8 +3,8 @@
 Об окончании ввода данных свидетельствует пустая строка.
 """
 
-# my_f = open("file.txt", "x")
-# str_list = ('\n'.join(list(input("Введите какие-нибудь данные: ").split())))
-# my_f.writelines(str_list)
-# my_f.close()
+my_f = open("file.txt", "x")
+str_list = ('\n'.join(list(input("Введите какие-нибудь данные: ").split())))
+my_f.writelines(str_list)
+my_f.close()
 
