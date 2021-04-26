b7ea6f4127a8585e5ed49032a541a04cfa38ed86
Slava# <72523263+vmenshikov106@users.noreply.github.com>
2021-04-26 11:34:50 +0300

    Update lesson_5_5.py

--- Python_hw_5/lesson_5_5.py
+++ Python_hw_5/lesson_5_5.py
@@ -1,12 +1,15 @@
-"""5. Создать (программно) текстовый файл, записать в него программно набор чисел, разделенных пробелами.
+"""Создать (программно) текстовый файл, записать в него программно набор чисел, разделенных пробелами.
 Программа должна подсчитывать сумму чисел в файле и выводить ее на экран
 """
 
-# with open("file_4.txt", "w") as my_f:
-#     num = f"{input('Введите числа через пробел: ')}"
-#     my_f.write(num)
-#
-# with open("file_4.txt", "r") as my_f:
-#     main_line = my_f.readline()
-#     main_list = map(int, main_line.split())
-#     print("Сумма введённых чисел: ", sum(main_list))
+
+with open("file_4.txt", "w") as my_f:
+    num = f"{input('Введите числа через пробел: ')}"
+    my_f.write(num)
+
+with open("file_4.txt", "r") as my_f:
+    main_line = my_f.readline()
+    main_list = map(int, main_line.split())
+    print("Сумма введённых чисел: ", sum(main_list))
+    
+    
